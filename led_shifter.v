9fc90db0010e9f3b0aab7d69bef82356fa37f453
Vinícius César Bertolo Calligaris <vinicius.cesar7@hotmail.com>
2025-06-03 18:41:27 -0300

    Teste 1

--- led_shifter.v
+++ led_shifter.v
@@ -1,10 +1,24 @@
 module LedShifter #(
-    parameter CLK_FREQ = 25_000_000 
+    parameter integer CLK_FREQ = 8 
 ) (
-    input  wire clk,
-    input  wire rst_n,
-    output  reg [7:0] leds
+    input  wire clk,       
+    input  wire rst_n,     
+    output reg [7:0] leds  
 );
+    reg [0:0] counter;  
 
-    
+    always @(posedge clk) begin
+        if (!rst_n) begin
+            counter <= 0;
+            leds    <= 8'b0001_1111; 
+        end else begin
+            if (counter == 1) begin
+                counter <= 0;
+                leds <= {leds[6:0], leds[7]};
+            end else begin
+                counter <= counter + 1;
+            end
+        end
+    end
+
 endmodule
